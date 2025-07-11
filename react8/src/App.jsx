54c46bc0277dac56e4508684335eca4f5a229c24
Sahil Sonkamble <150232278+sonkamblesahil@users.noreply.github.com>
2025-07-11 19:22:47 +0530

    learnt the useeffect hook and different variations of useeffect hook in react

--- react8/src/App.jsx
+++ react8/src/App.jsx
@@ -1,13 +1,68 @@
-
+import { useState } from "react";
+import { useEffect } from "react";
 
 function App() {
+  const [count, setCount] = useState(0);
+  const [total, setTotal] = useState(1);
+
+   const handleClickCount = () => {
+    setCount(count + 1);
+  };
+
+  const handleClickTotal = () => {
+    setTotal(total + 1);
+  };
+
+  //runs on every render
+  // useEffect(() => {
+
+  //     alert("this runs on eery render")
+  // })
+
+  //that runs on only first render
+  // useEffect(() => {
+
+  //   alert("this runs on only first render only")
+  //   return () => {
+  //   }
+  // }, [])
+
+  //single dependency render renders only when the count is changed
+  // useEffect(() => {
+  //  alert("this runs only whe count is updated")
+  // }, [count])
+
+  //multiple dependencies
+  // useEffect(() => {
+  //    alert("this runs when count or total is updated");
+  // }, [count,total]);
+
+ //learning cleanup function in useEffect
+ //purana wala component unmount hota hai aur fir naya wala component mount hota hai
+ useEffect(() => {
+    alert("count is updated");
+   return () => {
+     alert("count is unmounted from ui")
+     
+   }
+ }, [count])
  
+
 
   return (
     <>
-     
+      <div>
+        <div>
+          <button onClick={handleClickCount}>UpdateCount</button>
+          Count is :{count}
+        </div>
+        <div>
+          <button onClick={handleClickTotal}>UpdateTotal</button>
+          Total is :{total}
+        </div>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
